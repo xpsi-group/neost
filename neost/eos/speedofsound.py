6ffd06977b3fe0a3bb48f48e6fc2fa28aaae064f
Nathan Rutherford <76955751+nr1118@users.noreply.github.com>
2024-09-30 06:05:45 -0400

    Dark matter updates to the eos files
    Base.py includes:
    -a find_epsdm_cent function which computes the ADM central energy density corresponding to a given ADM fraction and baryonic central energy density. Then, solves it as  a root finding problem. 
    -a fchi_calc function, which computes the ADM mass-fraction for a given ADM central energy density and baryonic central energy density.
    
    Polytropes, speedofsound, tabulated includes:
    -Initialization of the Bosonic or Fermionic ADM equations of state using the Nelson et al 2018 ADM model.

--- neost/eos/speedofsound.py
+++ neost/eos/speedofsound.py
@@ -36,7 +36,13 @@ class SpeedofSoundEoS(BaseEoS):
         If True a low-density cEFT parameterization is used.
     ceft_method: str
         The name of the cEFT calculations used at low density.
-        Can be one of 'Hebeler', 'Drischler', 'Lynn' or 'Tews'.
+        Can be one of 'Hebeler', 'Drischler', 'Lynn', 'Keller-N2LO', 'Keller-N3L0', or 'Tews'.
+    adm_type: str
+        The name of the ADM particle type. Can be 'None', 'Bosonic', or 'Fermionic'
+    dm_halo: bool
+        If True, ADM halos will be allowed.
+    two_fluid_tidal: bool
+        If True, the two-fluid tidal deformability solver will be used. See TidalDef.py file
 
     Methods
     -------
@@ -46,6 +52,8 @@ class SpeedofSoundEoS(BaseEoS):
         Construct the crust of the equation of state, with or without cEFT.
     get_eos()
         Construct the high-density parameterization of the equation of state.
+    add_adm_eos()
+        Function to compute the ADM EoS, whether it be bosonic or fermionic in nature.
     plot()
         Plot the equation of state.
     plot_massradius()
@@ -53,7 +61,7 @@ class SpeedofSoundEoS(BaseEoS):
 
     """
 
-    def __init__(self, crust, rho_t):
+    def __init__(self, crust, rho_t,adm_type = 'None',dm_halo = False,two_fluid_tidal = False):
 
         super(SpeedofSoundEoS, self).__init__(crust, rho_t)
 
@@ -61,6 +69,16 @@ class SpeedofSoundEoS(BaseEoS):
         self.param_names = ['a1', 'a2', 'a3/a2', 'a4', 'a5']
         if self.ceft is True:
             self.param_names.append('ceft')
+
+        self.adm_type = adm_type
+        self.dm_halo = dm_halo
+        self.two_fluid_tidal = two_fluid_tidal
+
+        if self.adm_type not in ['Bosonic', 'Fermionic', 'None']:
+            raise TypeError('ADM model not recognized. Choose either "Bosonic" or "Fermionic". If ADM is not needed, select "None".')
+        
+        if self.adm_type in ['Bosonic','Fermionic']:
+            self.param_names +=['mchi','gchi_over_mphi', 'adm_fraction']
 
 
     def get_eos(self):
@@ -91,7 +109,8 @@ class SpeedofSoundEoS(BaseEoS):
 
         self.eos = UnivariateSpline(self.energydensities,
                                     self.pressures, k=1, s=0)
-
+        if self.adm_type in ['Bosonic', 'Fermionic']:
+            self.add_adm_eos()
 
     #######################
     # Auxiliary functions #
@@ -123,6 +142,98 @@ class SpeedofSoundEoS(BaseEoS):
         return (dmin * self._cs_crust(x * rho_ns) / c**2. +
                 dplus * (self.Cs_model_core(x, norm)))
 
+
+    def add_adm_eos(self):
+
+        def joinarray(ar1,ar2):
+            return np.concatenate((ar1,ar2))
+        
+        _c = 2.99792458e8  # m/s
+        _hbar = 1.0545718e-34  # J*s
+        MeV_to_Joules = 1.60218e-13  # J
+
+        self.mchi = self.eos_params['mchi'] * MeV_to_Joules / _c**2.
+        self.gchi_over_mphi = (self.eos_params['gchi_over_mphi'] / MeV_to_Joules * _c**2.)
+        self.adm_fraction = self.eos_params['adm_fraction']
+
+
+
+        # Bosonic Dark matter component
+        # geometric
+        if self.adm_type == 'Bosonic':
+            #print('Using bosonic EoS')
+            pos_nchi = np.logspace(-5, 55, 5000)
+            self.energydensities_dm = (self.mchi * _c**2 * pos_nchi + 1. / 2. *
+                                       pow(self.gchi_over_mphi,2) * _hbar**3 *
+                                       _c**(-1) * pos_nchi**2) *10 / c**2 #Factor of 10 is here to convert from SI base units to cgs base unit
+                                                                           # /c**2 is here since this is base units of g/(cm s^2) not g/cm^3
+            self.pressures_dm = (1. / 2. * pow(self.gchi_over_mphi,2) *
+                                 _hbar**3 * _c**(-1.) * pos_nchi**2) *10 #Factor of 10 is here to convert from SI base units to cgs base unit
+
+            self.massdensities_dm = pos_nchi * self.mchi * pow(10,-3) #Factor of 10^-3 is here to convert from SI base units to cgs base units
+
+        if self.adm_type == 'Fermionic':
+            #Fermionic Dark matter component
+            # geometric units
+
+            #print('Using fermionic EoS')
+            pts = 5000 #default point value
+  
+
+            lower_bound = -2.5 #default lower bound
+
+            upper_bound = 2.5 #default upper bound
+
+            if self.eos_params['mchi'] < 10:
+                upper_bound = 6
+           
+            if self.eos_params['mchi']>=pow(10,3):
+                lower_bound  = -14 
+        
+                upper_bound = 1
+                
+            if self.eos_params['mchi']>= pow(10,5):
+                lower_bound = -10
+                upper_bound = 1
+            
+
+            x= np.logspace(lower_bound,upper_bound,pts)
+
+
+            self.energydensities_dm = np.zeros(pts)
+
+            self.pressures_dm = np.zeros(pts)
+
+           
+            # APPROXIMATION EoS FOR VALUES OF x < 10**2
+            z = x[x<pow(10,-2)]
+            interact_term = 1./2.*pow(self.gchi_over_mphi,2)*self.mchi**2*z**6/(3*np.pi**2)**2
+
+            
+            self.energydensities_dm[0:len(z)] = (_c**5*self.mchi**4/_hbar**3*(z**3/(3*np.pi**2) + interact_term))*10 / c**2 #Factor of 10 is here to convert from SI base units to cgs base unit
+                                                                                                                     # /c**2 is here since this is base units of g/(cm s^2) not g/cm^3
+
+            self.pressures_dm[0:len(z)] = (_c**5*self.mchi**4/_hbar**3*(z**5/(15*np.pi**2) + interact_term))*10 #Factor of 10 is here to convert from SI base units to cgs base unit
+
+            # EXACT EoS FOR VALUES OF x >= 10**-2 
+            y = x[x>=pow(10,-2)]
+            const = (self.mchi**4*_c**5/(8*np.pi**2*_hbar**3))
+
+            A= const*(2*y**3+y)*np.sqrt(1+y**2)
+
+            B = const*np.log(y+np.sqrt(1+y**2))
+
+            C = (1/(18*np.pi**4))*pow(self.gchi_over_mphi,2)*(_c**5/_hbar**3)*(self.mchi*y)**6
+
+
+            self.energydensities_dm[len(z)::] = (A-B+C)*10 / c**2 #Factor of 10 is here to convert from SI base units to cgs base unit
+                                                           # /c**2 is here since this is base units of g/(cm s^2) not g/cm^3
+
+   
+            self.pressures_dm[len(z)::] = (const*np.sqrt(1+y**2)*((2./ 3.*y**3)-y)+B+C)*10 #Factor of 10 is here to convert from SI base units to cgs base unitw #10 ADDED ON 11/18. This is added to convert kg/ms^2 to g/cms^2
+
+            nchi = 1./(3*np.pi**2)*(self.mchi*_c*x/_hbar)**3
+            self.massdensities_dm = nchi * self.mchi * pow(10,-3) #Factor of 10^-3 is here to convert from SI base units to cgs base units
     
 
     def check_constraints(self):
@@ -146,6 +257,11 @@ class SpeedofSoundEoS(BaseEoS):
         if falling.size == 0:
             check = False
         if rising.size != 0 and falling.size != 0 and rising[0] < falling[0]:
+            check = False
+
+        if self.reach_fraction == True:
+            check = True
+        else:
             check = False
 
         return check
