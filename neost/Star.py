70b23d92cbffceb38c90f8816673880e67701c57
Isak Svensson <isak.svensson@physik.tu-darmstadt.de>
2024-10-09 22:42:57 +0200

    Fixed broken indentations 	modified:   neost/Star.py

--- neost/Star.py
+++ neost/Star.py
@@ -65,113 +65,110 @@ class Star():
 
 
         """
-        
+
         if self.enthalpy is True:
             if self.epscent_dm == 0.0:
                 self.Mb, self.Rns, self.tidal = solveTOVh(self.epscent,eps, pres)
             else:
                 raise Exception("two-fluid enthalpy solve currently not available")
                #two-fluid enthalpy tov solver not available currently
-               #self.Mb, self.Rns, self.tidal, self.Mdm_core, self.radius_dm_core = solveTOVdmh(self.epscent, self.epscent_dm, eps, pres, eps_dm, pres_dm) 
-            
+               #self.Mb, self.Rns, self.tidal, self.Mdm_core, self.radius_dm_core = solveTOVdmh(self.epscent, self.epscent_dm, eps, pres, eps_dm, pres_dm)
+
         else:
             if self.epscent_dm == 0.0:
                 self.Mb, self.Rns, self.tidal, self.Gtt = solveTOVr(self.epscent, eps, pres, atol, rtol, hmax, step)
-                
+
             else:
                 self.Mb, self.Rns, self.Mdm_core, self.Mdm_halo, self.radius_dm_core, self.radius_dm_halo, self.tidal = solveTOVdm(self.epscent, self.epscent_dm, eps, pres, eps_dm, pres_dm, dm_halo,two_fluid_tidal, atol, rtol, hmax, step)
 
 
-@property
-def Mrot(self):
-    """ Get the gravitational mass. """
-    return (self.Mb + self.Mdm_core + self.Mdm_halo) / Msun 
+    @property
+    def Mrot(self):
+        """ Get the gravitational mass. """
+        return (self.Mb + self.Mdm_core + self.Mdm_halo) / Msun
 
-@property
-def Mbaryonic(self):
-    """ Get the gravitational mass of baryonic component. """
-    return (self.Mb)/ Msun
+    @property
+    def Mbaryonic(self):
+        """ Get the gravitational mass of baryonic component. """
+        return (self.Mb)/ Msun
 
     @property
     def Req(self):
         """ Get the equatorial radius. """
         return (self.Rns) / 1e5
-@property
-def Mdm(self):
-    """ Get the total gravitational dark matter mass. """
-    return (self.Mdm_core + self.Mdm_halo) / Msun
 
-@property
-def Mdmcore(self):
-    """ Get the dark matter mass within the baryonic radius. """
-    return (self.Mdm_core) / Msun
+    @property
+    def Mdm(self):
+        """ Get the total gravitational dark matter mass. """
+        return (self.Mdm_core + self.Mdm_halo) / Msun
 
-@property
-def Mdmhalo(self):
-    """ Get the dark matter mass outside the baryonic radius. """
-    return (self.Mdm_halo) / Msun
+    @property
+    def Mdmcore(self):
+        """ Get the dark matter mass within the baryonic radius. """
+        return (self.Mdm_core) / Msun
 
-@property
-def Rdm(self):
-    """ Get the total dark matter radius."""
-    if self.radius_dm_halo == 0 or self.radius_dm_halo == 999e5:
-        Rdm = self.radius_dm_core
-        
-    else:
-        Rdm = self.radius_dm_halo
-    return (Rdm) / 1e5
+    @property
+    def Mdmhalo(self):
+        """ Get the dark matter mass outside the baryonic radius. """
+        return (self.Mdm_halo) / Msun
 
-@property
-def Rdm_core(self):
-    """ Get the dark matter core radius. """
-    return self.radius_dm_core  / 1e5
+    @property
+    def Rdm(self):
+        """ Get the total dark matter radius."""
+        if self.radius_dm_halo == 0 or self.radius_dm_halo == 999e5:
+            Rdm = self.radius_dm_core
 
-@property
-def Rdm_halo(self):
-    """ Get the dark matter halo radius. """
-    return self.radius_dm_halo  / 1e5
+        else:
+            Rdm = self.radius_dm_halo
+        return (Rdm) / 1e5
 
+    @property
+    def Rdm_core(self):
+        """ Get the dark matter core radius. """
+        return self.radius_dm_core  / 1e5
 
+    @property
+    def Rdm_halo(self):
+        """ Get the dark matter halo radius. """
+        return self.radius_dm_halo  / 1e5
 
+    def plot_structure(self):
+        """
+            Plot the internal mass and pressure structure of the star.
+        """
 
+        radius_grid_baryon = self.dist_baryon[:,0]/1e5
+        mass_dist_baryon = self.dist_baryon[:,1]/ Msun
+        pres_dist_baryon = self.dist_baryon[:,2]
+        fig, ax = pyplot.subplots(1,2, figsize=(14,6))
+        ax[0].plot(radius_grid_baryon, mass_dist_baryon,
+                   c='#005ABD', lw=2.5, label='Baryonic')
+        ax[1].plot(radius_grid_baryon, pres_dist_baryon, c='#005ABD',
+                   lw=2.5, label='Baryonic')
+        ax[1].set_yscale('log')
 
-def plot_structure(self):
-    """
-        Plot the internal mass and pressure structure of the star.
-    """
-    
-    radius_grid_baryon = self.dist_baryon[:,0]/1e5
-    mass_dist_baryon = self.dist_baryon[:,1]/ Msun
-    pres_dist_baryon = self.dist_baryon[:,2]
-    fig, ax = pyplot.subplots(1,2, figsize=(14,6))
-    ax[0].plot(radius_grid_baryon, mass_dist_baryon,
-               c='#005ABD', lw=2.5, label='Baryonic')
-    ax[1].plot(radius_grid_baryon, pres_dist_baryon, c='#005ABD',
-               lw=2.5, label='Baryonic')
-    ax[1].set_yscale('log')
-    
-    if self.epscent_dm!=0:
-        radius_grid_dm = self.dist_dm[:,0]/1e5
-        mass_dist_dm = self.dist_dm[:,1]/ Msun
-        pres_dist_dm = self.dist_dm[:,2]
-        ax[0].plot(radius_grid_dm, mass_dist_dm, c='#00B1B7', lw=2.5,
-               label='Dark matter')
-        ax[1].plot(radius_grid_dm,pres_dist_dm, c='#00B1B7',
-               lw=2.5, label='Dark matter')
+        if self.epscent_dm!=0:
+            radius_grid_dm = self.dist_dm[:,0]/1e5
+            mass_dist_dm = self.dist_dm[:,1]/ Msun
+            pres_dist_dm = self.dist_dm[:,2]
+            ax[0].plot(radius_grid_dm, mass_dist_dm, c='#00B1B7', lw=2.5,
+                   label='Dark matter')
+            ax[1].plot(radius_grid_dm,pres_dist_dm, c='#00B1B7',
+                   lw=2.5, label='Dark matter')
 
-    ax[1].set_ylim(1e18, 1e36)
-    ax[1].set_ylabel('p [dyn/cm$^{2}$]', fontsize=16)
-    ax[0].set_ylabel('m [M$_{\odot}$]', fontsize=16)
-    from matplotlib.ticker import ScalarFormatter
+        ax[1].set_ylim(1e18, 1e36)
+        ax[1].set_ylabel('p [dyn/cm$^{2}$]', fontsize=16)
+        ax[0].set_ylabel('m [M$_{\odot}$]', fontsize=16)
+        from matplotlib.ticker import ScalarFormatter
 
-    for i in range(2):
-        ax[i].tick_params(axis='both', which='major', labelsize=14)
-        ax[i].set_xlabel('r [km]', fontsize=16)
-        ax[i].set_xscale('log')
-        ax[i].set_xlim(0.9, 150)
-        ax[i].xaxis.set_major_formatter(ScalarFormatter())
-    ax[0].legend(prop={'size':16})
+        for i in range(2):
+            ax[i].tick_params(axis='both', which='major', labelsize=14)
+            ax[i].set_xlabel('r [km]', fontsize=16)
+            ax[i].set_xscale('log')
+            ax[i].set_xlim(0.9, 150)
+            ax[i].xaxis.set_major_formatter(ScalarFormatter())
+        ax[0].legend(prop={'size':16})
 
-    pyplot.savefig('Structure_plot.png',dpi = 300)
-    plt.tight_layout()
-    plt.show()
+        pyplot.savefig('Structure_plot.png',dpi = 300)
+        plt.tight_layout()
+        plt.show()
