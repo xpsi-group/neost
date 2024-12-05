222d40d68fb89eeff82160c288518e3891039104
Nathan Rutherford <76955751+nr1118@users.noreply.github.com>
2024-12-05 13:16:32 -0500

    Update test_likelihood.py
    maybe this fixes the spaces issue

--- tests/test_likelihood.py
+++ tests/test_likelihood.py
@@ -35,10 +35,9 @@ class TestLikelihoodCheck(object):
         par = prior.inverse_sample(cube)
         ll = likelihood.call(par)
         print(ll)
-        if np.isclose(-4.187612429216115, ll, rtol=1.0e-5) is False:
+        if (np.isclose(-4.187612429216115, ll, rtol=1.0e-5) is False):
             print('C TOV solvers either not installed or broken, using Python TOV solver instead')
-            assert np.isclose(--4.192614304543445, ll, rtol=1.0e-5)
-            
-        else:
+            assert np.isclose(-4.192614304543445, ll, rtol=1.0e-5)
+		else:
             assert np.isclose(-4.187612429216115, ll, rtol=1.0e-5)
 
