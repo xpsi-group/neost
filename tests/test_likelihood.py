a8a588e8f8d2a0f86dbd4e8de58a84faa3086afd
Nathan Rutherford <76955751+nr1118@users.noreply.github.com>
2024-12-05 13:03:21 -0500

    Update test_likelihood.py

--- tests/test_likelihood.py
+++ tests/test_likelihood.py
@@ -35,5 +35,9 @@ class TestLikelihoodCheck(object):
         par = prior.inverse_sample(cube)
         ll = likelihood.call(par)
         print(ll)
-        assert np.isclose(-4.187612429216115, ll, rtol=1.0e-5)
+	if np.isclose(-4.187612429216115, ll, rtol=1.0e-5) == False:
+		print('C TOV solvers either not installed or broken, using Python TOV solver instead')
+		assert np.isclose(--4.192614304543445, ll, rtol=1.0e-5)
+	else:
+		assert np.isclose(-4.187612429216115, ll, rtol=1.0e-5)
 
