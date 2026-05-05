9c675389a9a8e674d8fd965cc94638d231de2d7f
Isak Svensson <isak.svensson@tu-darmstadt.de>
2026-05-05 15:03:52 +0200

    Updated L_inf.py to read data files from the installed directory 	modified:   neost/L_inf.py

--- neost/L_inf.py
+++ neost/L_inf.py
@@ -13,20 +13,19 @@ energydensities = np.logspace(14.2, 16, 50)                # log of grams per ce
 number_density_ns = energydensities/rhons
 number_density = number_density_ns * n_ns
 
-# Load data ( for calculating the correlation, doesnt have to be done every time)
-#path = os.path.dirname(__file__)    # current path
-#file_path = os.path.abspath(os.path.join(path, '..', 'DANEoST/data/cEFT_band_fitting' ))
-file_path = '/work/home/mm12wyxy/neost/secretneost/DANEoST/data/cEFT_band_fitting'  # only hard-coding seems to work for travelling to paths outside of where neost is actually installed
+# Load data (for calculating the correlation, doesnt have to be done every time)
+path = os.path.dirname(__file__)    # current path, points to wherever this file is installed
+file_path = f'{path}/data'
 
-N3LO_beta = np.loadtxt(file_path+'/N3LO_beta_X2D.txt')                          ## add better path later
-N3LO_pnm = np.loadtxt(file_path+'/N3LO_PNM_X2D.txt')
-#N2LO_beta = np.loadtxt(file_path+'/N2LO_beta_X2D.txt')                         ## uncomment this line if working with N2LO   
-#N2LO_pnm = np.loadtxt(file_path+'/N2LO_PNM_X2D.txt')
+N3LO_beta = np.loadtxt(f'{file_path}/N3LO_beta_X2D.txt')
+N3LO_pnm = np.loadtxt(f'{file_path}/N3LO_PNM_X2D.txt')
+#N2LO_beta = np.loadtxt(f'{file_path}/N2LO_beta_X2D.txt')                         ## uncomment this line if working with N2LO
+#N2LO_pnm = np.loadtxt(f'{file_path}/N2LO_PNM_X2D.txt')
 
 samples = 1000                                            # they are very correlated, so a smaller array would also work
 quantiles = np.linspace(0.05,0.95, samples)
 densities = 0.157                                         # in the beta, pnm grids is closest density to n0 available
-    
+
 def linear(x, a, b):
     return a * x + b
 
@@ -45,7 +44,7 @@ def correlation(beta, pnm, density, quantiles, number_density):
     n_posterior = np.isclose(number_density, density, atol = 0.005)   #atol somewhat random, but more precision would barely change L inferred
     return n_posterior, popt_prior, pcov_data                         #pcov_data actually never used, but good for checking precision
 
-corr_N3LO = correlation(N3LO_beta, N3LO_pnm, densities, quantiles, number_density)   #don't forget to call N2LO if working with N2LO   
+corr_N3LO = correlation(N3LO_beta, N3LO_pnm, densities, quantiles, number_density)   #don't forget to call N2LO if working with N2LO
 
 class class_L_inf:
 
@@ -64,4 +63,4 @@ class class_L_inf:
         n = number_density[index_density]
         self.L = p_corr / n* 3
         return self.L
-  
+
